b9e62e22185269a9163aa2288dd2bb53506f36da
Tomas Herrera <tomasherrera91@hotmail.com>
2012-11-19 18:24:13 -0500

    Created Blog app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Malambito::Application.config.secret_token = '4505f22e2e5dd12b9425aa66f51bddbbea2c03b63b16418bc3360c0cbd65aea185cc560291c6d07160a726c4da7e00e7416f34a8c09757ca6adb965ab5b3822a'
